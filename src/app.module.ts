ae015d84b96628a941548ed47d7f767a4e8282f1
vinisooo <cviniciusliramdev@gmail.com>
2024-07-09 13:25:27 -0300

    install prisma and dotenv

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GoalsModule } from './goals/goals.module';
+import { ChecksModule } from './checks/checks.module';
+import { ChecksModule } from './modules/checks/checks.module';
+import { GoalsModule } from './modules/goals/goals.module';
 
 @Module({
-  imports: [],
+  imports: [GoalsModule, ChecksModule],
   controllers: [AppController],
   providers: [AppService],
 })
